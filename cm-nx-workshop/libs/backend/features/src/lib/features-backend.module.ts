f2332781d3f2fae1406167ef24e12bf56541a4e7
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-12-18 21:29:31 +0100

    enhance(dto, features): Update DTOs and backend feature modules

--- cm-nx-workshop/libs/backend/features/src/lib/features-backend.module.ts
+++ cm-nx-workshop/libs/backend/features/src/lib/features-backend.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth/auth.controller';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { AuthService } from './auth/auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  controllers: [],
-  providers: [],
-  exports: [],
+  imports: [JwtModule.register({
+    secret: 'secret',
+    signOptions: { expiresIn: '60m' },
+  })],
+  controllers: [AuthController, UserController],
+  providers: [UserService,AuthService],
+  exports: [UserService,AuthService],
 })
 export class FeaturesBackendModule {}
