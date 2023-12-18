3792a39adc12209a8b5cb2f0e34976eb9a39f4f0
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-12-18 21:28:04 +0100

    update(data-api): Modify app.module and main.ts for new features

--- cm-nx-workshop/apps/data-api/src/app/app.module.ts
+++ cm-nx-workshop/apps/data-api/src/app/app.module.ts
@@ -1,11 +1,9 @@
+import { FeaturesBackendModule } from './../../../../libs/backend/features/src/lib/features-backend.module';
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [FeaturesBackendModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- cm-nx-workshop/apps/data-api/src/main.ts
+++ cm-nx-workshop/apps/data-api/src/main.ts
@@ -5,13 +5,27 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
-
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ApiResponseInterceptor } from '@cm-nx-workshop/backend/dto';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  const corsOptions : CorsOptions ={};
+  app.enableCors(corsOptions);
+  app.useGlobalInterceptors(new ApiResponseInterceptor());
+  const config = new DocumentBuilder()
+  .setTitle('TourTitans Backend API')
+  .setDescription('The TourTitan API description')
+  .setVersion('1.0')
+  .addTag('TourTitan')
+  .addBearerAuth()  
+  .build();
+
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api-docs', app, document);
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
