6032db4df8694f856de1225cef1d533024d79132
Ámmar Almifalani <a.almifalani@student.avans.nl>
2023-12-19 23:55:20 +0100

    Update features-backend module

--- cm-nx-workshop/libs/backend/features/src/lib/features-backend.module.ts
+++ cm-nx-workshop/libs/backend/features/src/lib/features-backend.module.ts
@@ -4,12 +4,14 @@ import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { AuthService } from './auth/auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user/user.entity';
 
 @Module({
   imports: [JwtModule.register({
     secret: 'secret',
     signOptions: { expiresIn: '60m' },
-  })],
+  }),TypeOrmModule.forFeature([UserEntity])],
   controllers: [AuthController, UserController],
   providers: [UserService,AuthService],
   exports: [UserService,AuthService],
