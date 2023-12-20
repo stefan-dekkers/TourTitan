72dfceed27fae84fb925782f2d9d4d2e104a2c0c
Stefan <sw.dekkers@student.avans.nl>
2023-12-20 12:34:17 +0100

    Create Car (with Location) works with database

--- cm-nx-workshop/libs/backend/dto/src/lib/car.dto.ts
+++ cm-nx-workshop/libs/backend/dto/src/lib/car.dto.ts
@@ -1,47 +1,87 @@
-import { ICreateCar, ICreateLocation, IUpdateCar } from "@cm-nx-workshop/shared/api";
-import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import {
-    IsNotEmpty,
-    IsString,
-    IsOptional,
-    IsNumber,
-  } from 'class-validator';
+  ICreateCar,
+  ICreateLocation,
+  ILocation,
+  IUpdateCar,
+} from '@cm-nx-workshop/shared/api';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsString,
+  IsOptional,
+  IsNumber,
+  IsObject,
+} from 'class-validator';
 
-export class CreateCarDto implements ICreateCar{
-    @ApiProperty({ example: 'CruiseMobile', description: 'The name of the car' })
-    @IsNotEmpty()
-    @IsString()
-    name!: string;
-    @ApiProperty({ example: '01-CB-OL', description: 'The numberplate of the car' })
-    @IsNotEmpty()
-    @IsString()
-    plateNumber!: string;
-    @ApiProperty({ example: '4', description: 'The amount of people that fit in the car' })
-    @IsNumber()
-    @IsNotEmpty()
-    capacity!: number;
-    @ApiProperty({ example: 'Breda Office', description: 'The location of the car' })
-    @IsNotEmpty()
-    @IsString()
-    location!: string;
-    
+export class CreateCarDto implements ICreateCar {
+  @ApiProperty({ example: 'CruiseMobile', description: 'The name of the car' })
+  @IsNotEmpty()
+  @IsString()
+  name!: string;
+
+  @ApiProperty({
+    example: '01-CB-OL',
+    description: 'The numberplate of the car',
+  })
+  @IsNotEmpty()
+  @IsString()
+  plateNumber!: string;
+
+  @ApiProperty({
+    example: '4',
+    description: 'The amount of people that fit in the car',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  capacity!: number;
+
+  @ApiProperty({ example: '10000', description: 'The mileage of the car' })
+  @IsNumber()
+  @IsNotEmpty()
+  mileage!: number;
+
+  @ApiProperty({ example: 'true', description: 'The availability of the car' })
+  @IsNotEmpty()
+  isAvailable!: boolean;
+
+  @ApiProperty({
+    example: 'Breda Office',
+    description: 'The location of the car',
+  })
+  @IsNotEmpty()
+  @IsObject()
+  location!: ILocation;
 }
 
-export class UpdateCarDto implements IUpdateCar{
-    @ApiPropertyOptional({ example: 'CruiseMobile', description: 'The name of the car' })
-    @IsOptional()
-    @IsString()
-    name!: string;
-    @ApiPropertyOptional({ example: '01-CB-OL', description: 'The numberplate of the car' })
-    @IsOptional()
-    @IsString()
-    plateNumber!: string;
-    @ApiPropertyOptional({ example: '4', description: 'The amount of people that fit in the car' })
-    @IsNumber()
-    @IsOptional()
-    capacity!: number;
-    @ApiPropertyOptional({ example: '0', description: 'The location id of the car' })
-    @IsOptional()
-    @IsString()
-    location!: string;
+export class UpdateCarDto implements IUpdateCar {
+  @ApiPropertyOptional({
+    example: 'CruiseMobile',
+    description: 'The name of the car',
+  })
+  @IsOptional()
+  @IsString()
+  name!: string;
+
+  @ApiPropertyOptional({
+    example: '01-CB-OL',
+    description: 'The numberplate of the car',
+  })
+  @IsOptional()
+  @IsString()
+  plateNumber!: string;
+
+  @ApiPropertyOptional({
+    example: '4',
+    description: 'The amount of people that fit in the car',
+  })
+  @IsNumber()
+  @IsOptional()
+  capacity!: number;
+
+  @ApiPropertyOptional({
+    example: '0',
+    description: 'The location id of the car',
+  })
+  @IsOptional()
+  location!: ILocation;
 }
