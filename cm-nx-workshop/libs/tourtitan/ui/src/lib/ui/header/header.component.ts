208c1844a7bd36e0250dc2f6aad6405136ac9ef8
Stefan <sw.dekkers@student.avans.nl>
2023-12-20 16:35:57 +0100

    Added icon

--- cm-nx-workshop/libs/tourtitan/ui/src/lib/ui/header/header.component.ts
+++ cm-nx-workshop/libs/tourtitan/ui/src/lib/ui/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'cm-nx-workshop-header',
   templateUrl: './header.component.html',
   styles: [],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  imagePath?: string;
+
+  ngOnInit(): void {
+    this.imagePath = 'assets/img/tt-logo-128.png';
+  }
+}
