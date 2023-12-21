082fac127a517661f0e47bfc3aea267798c1229d
Sterre Vriens <sterre.vriens03@gmail.com>
2023-12-21 13:40:27 +0100

    started making delete modal

--- cm-nx-workshop/libs/tourtitan/features/src/lib/features/cars/cars-detail/cars-detail.component.ts
+++ cm-nx-workshop/libs/tourtitan/features/src/lib/features/cars/cars-detail/cars-detail.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ICar } from '@cm-nx-workshop/shared/api';
 import { CarsService } from '../cars.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'cm-nx-workshop-cars',
@@ -14,6 +15,7 @@ import { CarsService } from '../cars.service';
     subscription: Subscription | undefined = undefined;
   
     constructor(
+      private modalService: NgbModal,
       private carsService: CarsService,
       private route: ActivatedRoute,
       private router: Router
@@ -32,19 +34,38 @@ import { CarsService } from '../cars.service';
     }
   
     deleteCar(): void {
-      if (this.car?.id) {
-        this.carsService.delete(this.car).subscribe({
-          next: () => {
-            console.log('Car deleted successfully');
-            this.router.navigate(['/cars']); 
-          },
-          error: (error) => {
-            console.error('Error deleting car:', error);
-          },
+      // if (this.car?.id) {
+      //   this.carsService.delete(this.car).subscribe({
+      //     next: () => {
+      //       console.log('Car deleted successfully');
+      //       this.router.navigate(['/cars']); 
+      //     },
+      //     error: (error) => {
+      //       console.error('Error deleting car:', error);
+      //     },
+      //   });
+      // } else {
+      //   console.error('Car id is missing for deletion.');
+      // }
+
+        const modalRef = this.modalService.open(ProgressCreateComponent, { centered: true, backdrop: false });
+          
+        modalRef.componentInstance.progressCreated.subscribe((newProgress: IProgress) => {
+
+          newProgress.TrainingId=enrollment.TrainingId;
+          newProgress.UserId = this.currentUserID;
+          console.log('New Progress Created:', newProgress);
+              
+          this.userService.createProgress(newProgress).subscribe(
+            (succes) => {
+              console.log('Progress sucesfully created', succes)
+              this.location.go(this.location.path());
+            },
+            (error) => {
+              console.error('Error fetching user enrollments:', error);
+            }
+          );
         });
-      } else {
-        console.error('Car id is missing for deletion.');
-      }
     }
   
     ngOnDestroy(): void {
