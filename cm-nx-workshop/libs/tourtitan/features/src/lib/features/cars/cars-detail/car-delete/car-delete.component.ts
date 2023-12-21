959d54f2f07d63a3a205ed5678dd7fb8ed8a94c0
Sterre Vriens <sterre.vriens03@gmail.com>
2023-12-21 12:52:12 +0100

    Finished update

---
+++ cm-nx-workshop/libs/tourtitan/features/src/lib/features/cars/cars-detail/car-delete/car-delete.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'cm-nx-workshop-car-delete',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './car-delete.component.html',
+  styleUrl: './car-delete.component.css',
+})
+export class CarDeleteComponent {}
