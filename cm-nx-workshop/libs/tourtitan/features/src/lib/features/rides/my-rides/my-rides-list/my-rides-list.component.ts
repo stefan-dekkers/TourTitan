910a79f89cd08cce52a9fc89ed4f7e36e0801256
Ámmar Almifalani <a.almifalani@student.avans.nl>
2024-01-12 09:48:38 +0100

    Did nothing

--- cm-nx-workshop/libs/tourtitan/features/src/lib/features/rides/my-rides/my-rides-list/my-rides-list.component.ts
+++ cm-nx-workshop/libs/tourtitan/features/src/lib/features/rides/my-rides/my-rides-list/my-rides-list.component.ts
@@ -1,16 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { RidesService } from '../../rides.service';
 import { IRide } from '@cm-nx-workshop/shared/api';
-
 
 @Component({
   selector: 'cm-nx-workshop-my-rides',
   templateUrl: './my-rides-list.component.html',
   styles: [],
 })
-
 export class MyRidesListComponent implements OnInit, OnDestroy {
   ride: IRide[] | null = null;
   filteredRides: IRide[] | null = null;
@@ -31,33 +28,34 @@ export class MyRidesListComponent implements OnInit, OnDestroy {
   }
 
   filterRides(): void {
-    this.filteredRides = this.ride?.filter(ride =>
-      ride.arrivalLocation.street.toLowerCase().includes(this.searchTerm.toLowerCase())
-    ) || [];
+    this.filteredRides =
+      this.ride?.filter((ride) =>
+        ride.arrivalLocation.street
+          .toLowerCase()
+          .includes(this.searchTerm.toLowerCase())
+      ) || [];
   }
 
   formatDate(inputDate: Date): string {
     const date = new Date(inputDate);
-  
+
     // Ensure the input date is valid
     if (isNaN(date.getTime())) {
       throw new Error('Invalid date format');
     }
-  
+
     // Get date components
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
     const year = date.getFullYear();
-  
+
     // Get time components
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
-  
+
     // Construct the formatted date string
     const formattedDate = `${day}-${month}-${year} ${hours}:${minutes}`;
-  
+
     return formattedDate;
   }
-  
-  
 }
