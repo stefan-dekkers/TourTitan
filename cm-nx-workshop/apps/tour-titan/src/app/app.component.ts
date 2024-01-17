659f62c92ea77109b8cd87f16110375b2e56bea9
Ámmar Almifalani <a.almifalani@student.avans.nl>
2024-01-17 09:27:29 +0100

    add local_Id aap.component

--- cm-nx-workshop/apps/tour-titan/src/app/app.component.ts
+++ cm-nx-workshop/apps/tour-titan/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthModule } from '@cm-nx-workshop/tourtitan/auth';
 import { FeaturesModule } from '@cm-nx-workshop/tourtitan/features';
@@ -7,6 +7,7 @@ import { UiModule } from '@cm-nx-workshop/ui';
   standalone: true,
   imports: [FeaturesModule, RouterModule, UiModule, AuthModule],
   selector: 'cm-nx-workshop-root',
+  providers: [{ provide: LOCALE_ID, useValue: 'nl' }],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
