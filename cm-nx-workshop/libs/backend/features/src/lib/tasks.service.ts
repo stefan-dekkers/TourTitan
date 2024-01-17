e9acc05064b9161b1ad0518714c6c4c7e7aeb33f
Ámmar Almifalani <a.almifalani@student.avans.nl>
2024-01-18 00:45:35 +0100

    Fix task service to update ride  correctly

--- cm-nx-workshop/libs/backend/features/src/lib/tasks.service.ts
+++ cm-nx-workshop/libs/backend/features/src/lib/tasks.service.ts
@@ -5,24 +5,41 @@ import { Status } from '@cm-nx-workshop/shared/api';
 
 @Injectable()
 export class TasksService {
-  constructor(private readonly rideService: RideService) {}
   private readonly logger = new Logger(TasksService.name);
+
+  constructor(private readonly rideService: RideService) {}
+
   @Cron(CronExpression.EVERY_MINUTE)
   async checkAndUpdateRideStatuses() {
     this.logger.debug('Checking and updating ride statuses...');
 
     const currentDateTime = new Date();
-    currentDateTime.setHours(currentDateTime.getHours() + 1);
+    const utcCurrentDateTime = new Date(
+      currentDateTime.getTime() - currentDateTime.getTimezoneOffset() * 60000
+    );
+    this.logger.debug(
+      `Current UTC DateTime: ${utcCurrentDateTime.toISOString()}`
+    );
 
-    const rides = await this.rideService.findAll();
+    const rides = await this.rideService.findAllPending();
+
+    this.logger.debug(`Found ${rides.length} rides to check.`);
+
     for (const ride of rides) {
       const departureDateTime = new Date(ride.departureTime);
+      this.logger.debug(
+        `Departure UTC DateTime: ${departureDateTime.toISOString()}`
+      );
 
       if (
-        departureDateTime <= currentDateTime &&
+        departureDateTime <= utcCurrentDateTime &&
         ride.status === Status.PENDING
       ) {
+        this.logger.debug(`Updating status of ride with ID: ${ride.id}`);
         await this.rideService.updateStatus(ride.id);
+        this.logger.debug(`Updated status of ride with ID: ${ride.id}`);
+      } else {
+        this.logger.debug(`No update needed for ride with ID: ${ride.id}`);
       }
     }
   }
